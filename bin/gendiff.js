95234a260cf8bc30a165b6291ba5c63aa9206095
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-02-26 14:19:52 +0400

    Revert "create gendiff and add cli commander.js"
    This reverts commit a754b1a17cd8a6a6cafc6db0ecc348f18e2eca43.

--- bin/gendiff.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('commander');
-const program = new Command();
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program.parse();
