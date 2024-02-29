d1cf981106bc0c1e2a2f6b98f6b2ab0e31c9512f
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-02-29 20:33:41 +0400

    step 4 new logic

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 
 const readFile = (filePath) => {
@@ -11,17 +12,12 @@ const readFile = (filePath) => {
 const findDiff = (obj1, obj2) => {
 const keys1 = Object.keys(obj1);
 const keys2 = Object.keys(obj2);
-const diffrents  = keys1.map((key) => {
-    
-    if(!keys2.includes(key)){
-        return `-${key} = ${obj1[key]}`;
+const diffrents  = _.sortBy(_.union(keys1,keys2)).map((key) => {   
+    if(Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)){
+        return obj1[key] === obj2[key]? ` ${key} = ${obj1[key]}` : `- ${key} = ${obj1[key]}\n+ ${key} = ${obj2[key]}`
     }
-    else if(obj1[key] !== obj2[key]){
-        return `-${key} = ${obj1[key]} +${key} = ${obj2[key]}`;
-    }
-    return `${key} = ${obj1[key]}`;
-});
-
-return diffrents;
+    return `+ ${key} = ${obj2[key]}`
+})
+return `{\n${diffrents.join('\n')}\n}`;
 }
 export {readFile, findDiff};
