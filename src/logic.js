7d89ef6ca0d9dd676a9c2be86e1cb7b0efd72f05
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-02-29 22:08:36 +0400

    finish logic

--- src/logic.js
+++ src/logic.js
@@ -14,9 +14,10 @@ const keys1 = Object.keys(obj1);
 const keys2 = Object.keys(obj2);
 const diffrents  = _.sortBy(_.union(keys1,keys2)).map((key) => {   
     if(Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)){
-        return obj1[key] === obj2[key]? ` ${key} = ${obj1[key]}` : `- ${key} = ${obj1[key]}\n+ ${key} = ${obj2[key]}`
+        return obj1[key] === obj2[key]? `  ${key} = ${obj1[key]}` : `- ${key} = ${obj1[key]}\n+ ${key} = ${obj2[key]}`
     }
-    return `+ ${key} = ${obj2[key]}`
+
+    return obj1[key] === undefined?`+ ${key} = ${obj2[key]}` : `+ ${key} = ${obj1[key]}`;
 })
 return `{\n${diffrents.join('\n')}\n}`;
 }
