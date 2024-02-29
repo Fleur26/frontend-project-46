5231a05a112f87379c1a2a1c7f4c7e65b74e8a2d
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-02-29 18:35:22 +0400

    step 4

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
+
 const readFile = (filePath) => {
     const dirName = process.cwd(filePath);
     const fullPath = path.resolve(dirName, filePath);
@@ -10,10 +11,17 @@ const readFile = (filePath) => {
 const findDiff = (obj1, obj2) => {
 const keys1 = Object.keys(obj1);
 const keys2 = Object.keys(obj2);
-const diffrents  = {};
-for (let key of keys1){   
-diffrents[key] = obj1[key];
-}
+const diffrents  = keys1.map((key) => {
+    
+    if(!keys2.includes(key)){
+        return `-${key} = ${obj1[key]}`;
+    }
+    else if(obj1[key] !== obj2[key]){
+        return `-${key} = ${obj1[key]} +${key} = ${obj2[key]}`;
+    }
+    return `${key} = ${obj1[key]}`;
+});
+
 return diffrents;
 }
 export {readFile, findDiff};
