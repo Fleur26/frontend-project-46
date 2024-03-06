b40c54c455421fcd31b3aa5c6fc794252e5f28e2
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-03-06 15:43:10 +0400

    new logic

--- src/logic.js
+++ src/logic.js
@@ -18,16 +18,35 @@ const getFormat = (fileName) => {
 const findDiff = (obj1, obj2) => {
 const keys1 = Object.keys(obj1);
 const keys2 = Object.keys(obj2);
-const diffrents  = _.sortBy(_.union(keys1,keys2)).map((key) => {   
-    if(Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)){
-        return obj1[key] === obj2[key]? `  ${key} = ${obj1[key]}` : `- ${key} = ${obj1[key]}\n+ ${key} = ${obj2[key]}`
+//const diffrents  = _.sortBy(_.union(keys1,keys2)).map((key) => {   
+//    if(Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)){
+//        return obj1[key] === obj2[key]? `  ${key} = ${obj1[key]}` : `- ${key} = ${obj1[key]}\n+ ${key} = ${obj2[key]}`
+//    }
+//   else if(typeof key === 'object'){
+//       return `${key}: ${findDiff(key)}`;
+//    }
+//    return obj1[key] === undefined?`+ ${key} = ${obj2[key]}` : `+ ${key} = ${obj1[key]}`;
+//});
+//return `{\n${diffrents.join('\n')}\n}`;
+
+const diffrents  = _.sortBy(_.union(keys1,keys2)).map((key) => { 
+    if(!Object.hasOwn(obj1, key)){
+        return { type: 'added', key: key, val: obj2[key] };
     }
-    else if(typeof key === 'object'){
-        return `${key}: ${findDiff(key)}`;
+    else if (!Object.hasOwn(obj2, key)){
+        return { type: 'deleted', key: key, val: obj1[key] };
     }
-    return obj1[key] === undefined?`+ ${key} = ${obj2[key]}` : `+ ${key} = ${obj1[key]}`;
-})
-return `{\n${diffrents.join('\n')}\n}`;
+    else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object'){
+        return  { type: 'recursion', key: key, children: findDiff(obj1[key], obj2[key]) }
+    }
+    else if (obj1[key] !== obj2[key]){
+        return { type: 'changed', key: key, val: obj2[key] };
+    }
+    else if (obj1[key] !== obj2[key]){
+        return { type: 'unchanged', key: key, val: obj1[key] };
+    }
+});
+return diffrents;
 }
 
 
