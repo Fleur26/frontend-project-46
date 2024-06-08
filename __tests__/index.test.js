0706aad677843c13b623c21ff0b2a1790dd24b24
Fleur26 <lera.hoholeva@yandex.ru>
2024-06-08 18:39:01 +0400

    change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,22 +1,21 @@
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
-import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+const filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures', filename);
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-
-test('obj, json', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expected2.txt'));
-    });
-
-    test('obj, json, plain', () => {
-        expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expected2.txt'));
-        });
+test.each([
+  ['json', 'stylish', readFixture('expected1.txt')],
+  ['yml', 'stylish', readFixture('expected2txt')],
+  ['json', 'plain', readFixture('file1.json')],
+  ['yml', 'plain', readFixture('file1.yml')],
+  ['json', 'json', readFixture('file2.json')],
+  ['yml', 'json', readFixture('file2.yml')],
+])('all test gendiff', (extension, format, expected) => {
+  expect(genDiff(getFixturePath(`file1.${extension}`), getFixturePath(`file2.${extension}`), format)).toEqual(expected);
+});
