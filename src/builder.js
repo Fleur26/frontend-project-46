cde8dece9392f15337c55e889a4b43cad0130ccf
Fleur26 <lera.hoholeva@yandex.ru>
2024-06-24 22:14:14 +0400

    add function makeNode

--- src/builder.js
+++ src/builder.js
@@ -1,17 +1,15 @@
 import _ from 'lodash';
 
 export default function findDiff(obj1, obj2) {
-  const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2))).map((key) => {
-    const oldValue = obj1[key];
-    const newValue = obj2[key];
-    if (!_.has(obj2, key)) {
+  function makeNode(obj, obj0, oldValue, newValue, key) {
+    if (!_.has(obj0, key)) {
       return {
         action: 'deleted',
         key,
         oldValue,
       };
     }
-    if (!_.has(obj1, key)) {
+    if (!_.has(obj, key)) {
       return {
         action: 'added',
         key,
@@ -38,6 +36,11 @@ export default function findDiff(obj1, obj2) {
       key,
       oldValue,
     };
+  }
+  const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2))).map((key) => {
+    const oldValue = obj1[key];
+    const newValue = obj2[key];
+    return makeNode(obj1, obj2, oldValue, newValue, key);
   });
   return allKeys;
 }
