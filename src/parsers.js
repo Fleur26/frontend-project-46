548a5d1c9d70071eb96f6b5d1d3e7db36ef057c5
Fleur26 <lera.hoholeva@yandex.ru>
2024-06-25 13:18:39 +0400

    add Dispatching by key in file parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,16 @@
 import yaml from 'js-yaml';
 
 const parse = (file, format) => {
-  switch (format) {
-    case 'json':
-      return JSON.parse(file);
-    case 'yml':
-      return yaml.load(file);
-    default:
-      throw new Error(`Format file ${file} is not correct`);
+  const parsers = {
+    json: JSON.parse,
+    yml: yaml.load,
+  };
+
+  if (parsers[format]) {
+    return parsers[format](file);
   }
+
+  throw Error(`unknow type ${format}`);
 };
 
 export default parse;
