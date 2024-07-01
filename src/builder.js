faa7314f33fd57bba4eae6545f904fbb691f7d28
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-01 15:07:04 +0400

    return to prevuis version

--- src/builder.js
+++ src/builder.js
@@ -1,30 +1,46 @@
 import _ from 'lodash';
 
 export default function findDiff(obj1, obj2) {
-  const makeNode = (action, key, oldValue, newValue, children) => ({
-    action,
-    key,
-    oldValue,
-    newValue,
-    children,
-  });
+  function makeNode(obj, obj0, oldValue, newValue, key) {
+    if (!_.has(obj0, key)) {
+      return {
+        action: 'deleted',
+        key,
+        oldValue,
+      };
+    }
+    if (!_.has(obj, key)) {
+      return {
+        action: 'added',
+        key,
+        newValue,
+      };
+    }
+    if (_.isObject(oldValue) && _.isObject(newValue)) {
+      return {
+        action: 'nested',
+        key,
+        children: findDiff(oldValue, newValue),
+      };
+    }
+    if (oldValue !== newValue) {
+      return {
+        action: 'changed',
+        key,
+        oldValue,
+        newValue,
+      };
+    }
+    return {
+      action: 'unchanged',
+      key,
+      oldValue,
+    };
+  }
   const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2))).map((key) => {
     const oldValue = obj1[key];
     const newValue = obj2[key];
-    if (!_.has(obj1, key)) {
-      return makeNode('deleted', key, oldValue);
-    }
-    if (!_.has(obj2, key)) {
-      return makeNode('added', key, newValue);
-    }
-    if (_.isObject(oldValue) && _.isObject(newValue)) {
-      const children = findDiff(oldValue, newValue);
-      return makeNode('nested', key, children);
-    }
-    if (oldValue !== newValue) {
-      return makeNode('changed', key, oldValue, newValue);
-    }
-    return makeNode('unchanged', key, oldValue);
+    return makeNode(obj1, obj2, oldValue, newValue, key);
   });
   return allKeys;
 }
